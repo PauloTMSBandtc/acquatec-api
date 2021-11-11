b9b2cfbeb23ff5c233fe3eea29c9667cad9a958b
Paulo Souza <pauloga2@live.com>
2021-11-11 02:03:55 -0300

    adicionando api de captura de dados

--- api-site/src/models/usuarioModel.js
+++ api-site/src/models/usuarioModel.js
@@ -21,7 +21,7 @@ function entrar(email, senha) {
 function cadastrar(nome, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+        INSERT INTO sensores (temperatura, data) VALUES ('${nome}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
